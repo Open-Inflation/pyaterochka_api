ce9d8ff12de82e223ffff810849c9046a9662034
Miskler <69055489+Miskler@users.noreply.github.com>
2025-11-15 18:53:58 +0300

    Update __init__.py

--- pyaterochka_api/__init__.py
+++ pyaterochka_api/__init__.py
@@ -1,5 +1,5 @@
 from .enums import PurchaseMode, Sorting
 from .manager import PyaterochkaAPI
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __all__ = ["PyaterochkaAPI", "PurchaseMode", "Sorting"]
